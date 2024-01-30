6c629ca520b000863eb5381a5546a00cf3354aad
poojamistry30 <54638390+poojamistry30@users.noreply.github.com>
2024-01-30 13:57:11 +0530

    Update Weather Observation Station 6.sql

--- Select/Weather Observation Station 6.sql
+++ Select/Weather Observation Station 6.sql
@@ -13,8 +13,7 @@ Given a table STATION that holds data for five fields namely ID, CITY, STATE, NO
 +-------------+------------+
 
 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
-*/
 --Solution
 
 SELECT DISTINCT CITY FROM STATION
-WHERE SUBSTRING(CITY,1,1) IN ('a','e','i','o','u');
+WHERE SUBSTRING(CITY,1,1) IN ('a','e','i','o','u')
